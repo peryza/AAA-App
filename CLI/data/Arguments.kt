9aa229b8ab0bbc69fa1dbea7f51c6b9df56f0309
legion <slegionv@yandex.ru>
2020-11-03 16:47:16 +0300

    В обертку Arguments добавлены методы проверки необходимости аутентификации, авторизации, аккаунтинга.

--- CLI/data/Arguments.kt
+++ CLI/data/Arguments.kt
@@ -1,17 +1,26 @@
 package data
 
 data class Arguments(
-    var h: Boolean=false,
-    var login: String?=null,
-    var pass: String?=null,
-    var role: String?=null,
-    var res: String?=null,
-    var ds: String?=null,
-    var de: String?=null,
-    var vol: String?=null) {
+    var h: Boolean = false,
+    var login: String? = null,
+    var pass: String? = null,
+    var role: String? = null,
+    var res: String? = null,
+    var ds: String? = null,
+    var de: String? = null,
+    var vol: String? = null
+) {
 
     fun hasHelp() = h
 
-    fun isEmpty() = (login == null && pass == null && role == null &&
-            res == null && ds == null && de == null && vol == null)
+    fun isEmpty() =
+        (login == null && pass == null && role == null && res == null && ds == null && de == null && vol == null)
+
+    fun hasNeedHelp() = hasHelp() || isEmpty() || !hasNeedAuthentication()
+
+    fun hasNeedAuthentication() = login != null && pass != null
+
+    fun hasNeedAuthorization() = role != null && res != null
+
+    fun hasNeedAccounting() = ds != null && de != null && vol != null
 }
