8d35cea121a8570e2f9eb047d5923e3367c43abb
legion <slegionv@yandex.ru>
2020-11-03 16:44:02 +0300

    Рефакторинг кода обертки БД.

--- CLI/services/DatabaseWrapper.kt
+++ CLI/services/DatabaseWrapper.kt
@@ -1,24 +1,25 @@
 package services
 
-import data.*
-import db.*
+import data.Activity
+import data.RoleResource
+import data.User
+import db.tableActivity
+import db.tableRolesResources
+import db.tableUsers
 
 class DatabaseWrapper {
 
     fun getUser(login: String): User {
-        val response = tableUsers.find { it["login"] == login }
-        if (response == null) {
-            terminate(exitCode = ExitCodes.UnknownLogin.exitCode, printHelp = false)
-        }
-        val id: Int = response!!.getValue("id").toInt()
+        val response = tableUsers.find { it["login"] == login } ?: return User()
+        val id: Long = response.getValue("id").toLong()
         val hashPassword: String = response.getValue("hashPassword")
         val salt: String = response.getValue("salt")
         return User(id, login, hashPassword, salt)
     }
 
-    private fun checkAccess(roleResource: RoleResource): Boolean {
+    fun checkAccess(roleResource: RoleResource): Boolean {
         val response = tableRolesResources.filter {
-            it["role"] == roleResource.role && it.getValue("idUser").toInt() == roleResource.idUser
+            it["role"] == roleResource.role && it.getValue("idUser").toLong() == roleResource.idUser
                     && checkResourceAccess(roleResource.resource, it.getValue("resource"))
         }
         if (response.isEmpty())
