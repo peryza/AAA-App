392d30aeef88ce033bbc86ab23206e5f6ae3d1fc
legion <slegionv@yandex.ru>
2020-11-03 16:44:50 +0300

    В обертку Activity добавлен метод проверки формата даты и объема.

--- CLI/data/Activity.kt
+++ CLI/data/Activity.kt
@@ -1,8 +1,12 @@
 package data
 
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
+
 data class Activity(
-    val id: Int,
-    val idRoleResource: Int,
+    val id: Long? = null,
+    val role: String,
+    val res: String,
     val ds: String,
     val de: String,
     val vol: String
@@ -11,10 +15,23 @@ data class Activity(
     fun toMap(): Map<String, String> {
         return mapOf(
             "id" to id.toString(),
-            "idRoleResource" to idRoleResource.toString(),
+            "role" to role,
+            "res" to res,
             "ds" to ds,
             "de" to de,
             "vol" to vol
         )
+    }
+
+    fun hasValidData(): Boolean {
+        try {
+            LocalDate.parse(ds)
+            LocalDate.parse(de)
+        } catch (error: DateTimeParseException) {
+            return false
+        }
+        if (vol.toIntOrNull() == null)
+            return false
+        return true
     }
 }
