47e043abfc2a252f233452f6635ad37262576d27
legion <slegionv@yandex.ru>
2020-11-09 12:29:42 +0300

    Пункт 1. Добавление именнованных аргументов.

--- src/db/DataMap.kt
+++ src/db/DataMap.kt
@@ -6,19 +6,18 @@ import data.Roles.*
 import data.User
 
 val tableUsers = mutableListOf(
-    User(1, "vasya", "7a5a73db77e24a3964fa333fd43be8bb", "bv5PehSMfV11Cd"),
-    User(2, "admin", "b43cfeda0e8e4bd96561535db8a1d377", "QxLUF1bgIAdeQX"),
-    User(3, "q", "4a220600490d41af793cbb5c4494e435", "YYLmfY6IehjZMQ")
+    User(id = 1, login = "vasya", hashPassword = "7a5a73db77e24a3964fa333fd43be8bb", salt = "bv5PehSMfV11Cd"),
+    User(id = 2, login = "admin", hashPassword = "b43cfeda0e8e4bd96561535db8a1d377", salt = "QxLUF1bgIAdeQX"),
+    User(id = 3, login = "q", hashPassword = "4a220600490d41af793cbb5c4494e435", salt = "YYLmfY6IehjZMQ")
 )
 
 val tableRolesResources = mutableListOf(
-    RoleResource(1, READ, "A", 1),
-    RoleResource(2, WRITE, "A.B.C", 1),
-    RoleResource(3, EXECUTE, "A.B", 2),
-    RoleResource(4, EXECUTE, "B", 3),
-    RoleResource(5, READ, "A.B.C", 2),
-    RoleResource(6, WRITE, "A.B", 2),
-    RoleResource(7, READ, "A", 2)
+    RoleResource(id = 1, role = READ, resource = "A", idUser = 1),
+    RoleResource(id = 2, role = WRITE, resource = "A.B.C", idUser = 1),
+    RoleResource(id = 3, role = EXECUTE, resource = "A.B", idUser = 2),
+    RoleResource(id = 4, role = EXECUTE, resource = "B", idUser = 3),
+    RoleResource(id = 5, role = READ, resource = "A.B.C", idUser = 2),
+    RoleResource(id = 6, role = WRITE, resource = "A.B", idUser = 2),
+    RoleResource(id = 7, role = READ, resource = "A", idUser = 2)
 )
-
 val tableActivity: MutableList<Activity> = mutableListOf()
